7fdb1c1f8bd6710127cff312d2ecccf6e579ac16
Esteban <you.azt@gmail.com>
2022-09-26 09:13:01 -0500

    Ajuste primer commit

---
+++ Ws/CasaSubastas/src/main/java/co/edu/uniquindio/casasubastas/HelloApplication.java
@@ -0,0 +1,27 @@
+package co.edu.uniquindio.casasubastas;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        URL url = new File("src/main/java/co/edu/uniquindio/casasubastas/views/hello-view.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ Ws/CasaSubastas/src/main/java/co/edu/uniquindio/casasubastas/model/services/ClasePrueba.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.casasubastas.model.services;
+
+public class ClasePrueba {
+}
