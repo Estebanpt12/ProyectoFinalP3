87bd954577f5e22795496874622ccf9d36e8c675
Cdrian18 <valenciacarlos1218@gmail.com>
2022-10-13 18:49:27 -0500

    V0.1.3

--- Ws/CasaSubastas/src/main/java/co/edu/uniquindio/casasubastas/controllers/gui3Controller.java
+++ Ws/CasaSubastas/src/main/java/co/edu/uniquindio/casasubastas/controllers/gui3Controller.java
@@ -1,14 +1,97 @@
 package co.edu.uniquindio.casasubastas.controllers;
 
+import co.edu.uniquindio.casasubastas.exceptions.EmptyFieldsException;
+import co.edu.uniquindio.casasubastas.exceptions.UserNotLoggedException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
 
 public class gui3Controller {
-    @FXML
-    private Label welcomeText;
 
+    /**
+     * Botón para crear Anuncio
+     * */
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btnCrearAnuncio;
+    /**
+     * DatePicker para escoger la fecha en la que el anuncio termina
+     * */
+    @FXML
+    private DatePicker dpFechaFin;
+    /**
+     * Tipo de anuncio correspondiente a bien raíz
+     * */
+    @FXML
+    private MenuItem miBienRaiz;
+    /**
+     * Tipo de anuncio correspondiente a Deportes
+     */
+    @FXML
+    private MenuItem miDeportes;
+    /**
+     *Tipo de anuncio correspondiente a Hogar
+     */
+    @FXML
+    private MenuItem miHogar;
+    /**
+     *Tipo de anuncio correspondiente a Tecnología
+     */
+    @FXML
+    private MenuItem miTecnologia;
+    /**
+     *Tipo de anuncio correspondiente a Vehiculos
+     */
+    @FXML
+    private MenuItem miVehiculos;
+    /**
+     *Menú desplegable con los tipos de anuncios
+     */
+    @FXML
+    private MenuButton mnbTipoProducto;
+    /**
+     *Label del titulo
+     */
+    @FXML
+    private Label titleLabel;
+    /**
+     * Área de texto para la descripción del producto
+     * */
+    @FXML
+    private TextArea txtArDescripción;
+    /**
+     * TextField para el nombre del producto
+     */
+    @FXML
+    private TextField txtNombreProducto;
+    /**
+     *TextField para la ruta de la imagen
+     */
+    @FXML
+    private TextField txtRutaImagen;
+
+    /**
+     *Método para crear un anuncio al oprimir el botón
+     *@param event Evento de pulsar el botón
+     *@throws UserNotLoggedException Excepción al intentar crear un anuncio sin haber hecho el login
+     */
+    @FXML
+    void crearAnuncio(ActionEvent event) {
+        try {
+            Singleton.crearAnuncio();
+        }catch(UserNotLoggedException ue) {
+            throw ue;
+        }catch(EmptyFieldsException ef){
+            JOptionPane.showMessageDialog(null,"Hay campos vacíos");
+        }
     }
+    }
+
 }
--- Ws/CasaSubastas/src/main/java/co/edu/uniquindio/casasubastas/controllers/gui4Controller.java
+++ Ws/CasaSubastas/src/main/java/co/edu/uniquindio/casasubastas/controllers/gui4Controller.java
@@ -1,14 +1,68 @@
+
 package co.edu.uniquindio.casasubastas.controllers;
 
+
+import co.edu.uniquindio.casasubastas.exceptions.EmptyFieldsException;
+import co.edu.uniquindio.casasubastas.exceptions.ExistingUserException;
+import co.edu.uniquindio.casasubastas.exceptions.InvalidUserException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class gui4Controller {
-    @FXML
-    private Label welcomeText;
 
+    /**
+     * Botón de Regrisrto
+     */
+    @FXML // fx:id="btnRegistro"
+    private Button btnRegistro; // Value injected by FXMLLoader
+    /**
+     * TextField de la edad
+     */
+    @FXML // fx:id="edadTextField"
+    private TextField edadTextField; // Value injected by FXMLLoader
+    /**
+     * TextField del nombre
+     */
+    @FXML // fx:id="nombreTextField"
+    private TextField nombreTextField; // Value injected by FXMLLoader
+    /**
+     * TextField de la contraseña
+     */
+    @FXML // fx:id="passwordTextField"
+    private PasswordField passwordTextField; // Value injected by FXMLLoader
+    /**
+     * Label del título de la ventana
+     */
+    @FXML // fx:id="titleLabel"
+    private Label titleLabel; // Value injected by FXMLLoader
+    /**
+     * TextField del nombre de usuario
+     */
+    @FXML // fx:id="usernameTextField"
+    private TextField usernameTextField; // Value injected by FXMLLoader
+
+    /**
+     * Método para crear comprador al oprimir el botón de registro
+     * @param event evento de oprimir el botón
+     * @throws InvalidUserException Válida si el usuario es válido
+     * @throws ExistingUserException Válida si el usuario que se intenta registrar ya existe
+     */
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void crearComprador(ActionEvent event) throws InvalidUserException,ExistingUserException{
+        try{
+            Singleton.crearComprador();
+        }catch (EmptyFieldsException ef){
+            JOptionPane.showMessageDialog(null,"Hay campos vacíos");
+        }catch (InvalidUserException ie){
+            throw ie;
+        }catch (ExistingUserException ex){
+            throw ex;
+        }
     }
 }
