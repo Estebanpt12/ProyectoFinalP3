c9516f5127164fee65c3f9a5325f839046da8b6b
Marlon Espinosa <pulgita-0714@hotmail.com>
2022-10-17 18:36:32 -0500

    Interfaces Anunciante, Comprador

--- Ws/CasaSubastas/src/main/java/co/edu/uniquindio/casasubastas/application/CasaSubastas.java
+++ Ws/CasaSubastas/src/main/java/co/edu/uniquindio/casasubastas/application/CasaSubastas.java
@@ -11,7 +11,17 @@ import java.io.IOException;
 import java.net.URL;
 
 public class CasaSubastas extends Application {
+    private Stage primaryStage;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
+<<<<<<< Updated upstream
     public void start(Stage stage) throws IOException {
         URL url = new File("src/main/java/co/edu/uniquindio/casasubastas/views/login-view.fxml").toURI().toURL();
         Parent root = FXMLLoader.load(url);
@@ -19,9 +29,38 @@ public class CasaSubastas extends Application {
         stage.setTitle("LogIn");
         stage.setScene(scene);
         stage.show();
+=======
+    public void start (Stage primaryStage) throws Exception {
+
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Casa Subasta");
+        mostrarVentanaPrincipal();
+>>>>>>> Stashed changes
     }
 
-    public static void main(String[] args) {
-        launch();
+    public void mostrarVentanaPrincipal(){
+
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Aplication.class.getResource("views/main-view.fxml"));
+            AnchorPane root = (AnchorPane) loader.load();
+            //
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+
 }
